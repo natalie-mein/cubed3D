cb43bb9b549e4e16f0a683931ea739d8be764f75
Natalie Meintjes <nmeintje@c1r5p6.hive.fi>
2025-04-10 12:59:42 +0300

    main was not commited for some reason

--- sources/main.c
+++ sources/main.c
@@ -10,7 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "../include/cub3D.h"
+#include "cub3D.h"
+
+// exit() is allowed in the project
+// so the program might stop at void functions
+int	main(int argc, char **argv)
+{
+	t_game	*game;
+	char	*file_name;
+
+	(void)argc;
+	game = NULL;
+	file_name = argv[1];
+	//validate_input(argc, argv[1]); // no memory freeing necessary on error.
+	parse_map(file_name, game);
+	//	mlx_set_setting(MLX_MAXIMIZED, true);
+	
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "~ CUB3D ~", true);
+	if (!mlx)
+		ft_error();
+
+	/* Do stuff */
+
+	// Create and display the image.
+	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+
+	// Even after the image is being displayed, we can still modify the buffer.
+	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
+
+	// Register a hook and pass mlx as an optional param.
+	// NOTE: Do this before calling mlx_loop!
+	//	mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	free_game(game); // free the game struct.
+	return (SUCCESS);
+}
+/* // Natalie Main
 #include "raycasting.h"
 
 int	main(void)
@@ -26,47 +64,4 @@ int	main(void)
 	//printf("after mlx_loop\n");
 	exit_game(&data);
 	return (0);
-}
-
-/*// exit() is allowed in the project
-// so the program might stop at void functions
-int	main(int argc, char **argv)
-{
-	(void)argv;
-
-	if (argc != 2)
-		help_msg();
-	if (argc == 2)
-		ft_file_type(argv[1]);
-//	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
-	if (!mlx)
-		ft_error();
-
-	/* Do stuff */
-
-	// Create and display the image.
-	data->image = mlx_new_image(data->mlx, 256, 256);
-	if (!data->image || (mlx_image_to_window(data->mlx, data->image, 0, 0) < 0))
-		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(data->image, 0, 0, 0xFF0000FF);*/
-	/*char *map[] = {
-        "111111",
-        "100001",
-        "101101",
-        "100001",
-        "111111",
-        NULL
-    }; 
-
-    draw_map(data, map);
-
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
-//	mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return (0);
-}
+} */
