e73b97b6728d765902b86acb31d8a9b4d9d4c369
Martin Dahlstrom-Heuser <mdahlstr@c1r6p15.hive.fi>
2025-04-30 13:21:52 +0300

    merge conflicts

--- sources/parsing_utils1.c
+++ sources/parsing_utils1.c
@@ -6,7 +6,11 @@
 /*   By: mdahlstr <mdahlstr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:35:24 by mdahlstr          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/04/30 13:18:49 by mdahlstr         ###   ########.fr       */
+=======
+/*   Updated: 2025/04/24 18:07:15 by mdahlstr         ###   ########.fr       */
+>>>>>>> a5bdf83 (test file works even with lots of spaces)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,8 +115,8 @@ int get_fd(char *file_name, t_data *data)
 	int	fd;
 
 	(void)file_name;
-	fd = open("maps/simple_map.cub", O_RDONLY); /////////////////////////////////////////
-	//fd = open("maps/valid_maps/test.cub", O_RDONLY);
+	//fd = open("maps/simple_map.cub", O_RDONLY); /////////////////////////////////////////
+	fd = open("maps/valid_maps/test.cub", O_RDONLY);
 	//fd = open("maps/valid_maps/test2.cub", O_RDONLY);
 	//// invalid maps: /////////////////////////////////////////////////////////
 	fd = open("maps/valid_maps/test.cub", O_RDONLY);
